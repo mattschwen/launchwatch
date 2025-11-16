b71e6213e107d66d9547fdfd0f98cb533aae5933
Matt Schwen <mschwen@gmail.com>
2025-11-15 17:52:17 -0700

    fix(config): add external image domains for Space Devs API
    Configure Next.js Image component to allow images from external sources:
    - thespacedevs-prod.nyc3.digitaloceanspaces.com (Launch Library 2)
    - spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com (Space Launch Now)
    - images2.imgbox.com (Community images)
    - i.imgur.com (Imgur hosted images)
    
    Fixes runtime error: "hostname is not configured under images"
    
    This allows LaunchCard and other components to display rocket and
    launch images from the Space Devs API using Next.js optimized Image
    component without configuration errors.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'thespacedevs-prod.nyc3.digitaloceanspaces.com',
+        pathname: '/media/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com',
+        pathname: '/media/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images2.imgbox.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
